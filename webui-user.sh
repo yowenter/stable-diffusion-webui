6c91fb27ed1eb76740b7819b1e20ce8a58886649
debug <you@example.com>
2023-07-20 12:44:44 +0800

    add acclerate

--- webui-user.sh
+++ webui-user.sh
@@ -49,6 +49,46 @@ export REQS_FILE="requirements_versions.txt"
 export http_proxy=http://10.50.26.95:8118
 export https_proxy=http://10.50.26.95:8118
 
- export no_proxy=localhost
+export no_proxy=localhost
+
+# # Try using TCMalloc on Linux
+# prepare_tcmalloc() {
+#     if [[ "${OSTYPE}" == "linux"* ]] && [[ -z "${NO_TCMALLOC}" ]] && [[ -z "${LD_PRELOAD}" ]]; then
+#         TCMALLOC="$(PATH=/usr/sbin:$PATH ldconfig -p | grep -Po "libtcmalloc(_minimal|)\.so\.\d" | head -n 1)"
+#         if [[ ! -z "${TCMALLOC}" ]]; then
+#             echo "Using TCMalloc: ${TCMALLOC}"
+#             export LD_PRELOAD="${TCMALLOC}"
+#         else
+#             printf "\e[1m\e[31mCannot locate TCMalloc (improves CPU memory usage)\e[0m\n"
+#         fi
+#     fi
+# }
+
+# KEEP_GOING=1
+# export SD_WEBUI_RESTART=tmp/restart
+# while [[ "$KEEP_GOING" -eq "1" ]]; do
+#     if [[ ! -z "${ACCELERATE}" ]] && [ ${ACCELERATE}="True" ] && [ -x "$(command -v accelerate)" ]; then
+#         printf "\n%s\n" "${delimiter}"
+#         printf "Accelerating launch.py..."
+#         printf "\n%s\n" "${delimiter}"
+#         prepare_tcmalloc
+#         accelerate launch --num_cpu_threads_per_process=6 "${LAUNCH_SCRIPT}" "$@"
+#     else
+#         printf "\n%s\n" "${delimiter}"
+#         printf "Launching launch.py..."
+#         printf "\n%s\n" "${delimiter}"
+#         prepare_tcmalloc
+#         "${python_cmd}" "${LAUNCH_SCRIPT}" "$@"
+#     fi
+
+#     if [[ ! -f tmp/restart ]]; then
+#         KEEP_GOING=0
+#     fi
+# done
+
+
+# if use acclerate
+accelerate launch --num_processes=1 --num_cpu_threads_per_process=1  launch.py --listen  --port 9092 --enable-insecure-extension-access
  
-python3 launch.py --listen  --port 9092 --enable-insecure-extension-access
+# else use 
+#python3 launch.py --listen  --port 9092 --enable-insecure-extension-access
