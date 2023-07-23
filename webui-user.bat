4e090c72d151a7c0823362793e23316f599f32aa
TaoGe <6657718+yowenter@users.noreply.github.com>
2023-07-23 18:56:31 +0800

    windows launch

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=.\venv\Scripts\python.exe
 set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set VENV_DIR=venv
+set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers --no-half
 
 call webui.bat
