ac252a7c91267c27f42f26f4a0853237053ca6c1
TaoGe <6657718+yowenter@users.noreply.github.com>
2023-07-23 23:48:55 +0800

    add controlnet txt2img api

--- modules/api/models.py
+++ modules/api/models.py
@@ -107,6 +107,7 @@ StableDiffusionTxt2ImgProcessingAPI = PydanticModelGenerator(
         {"key": "send_images", "type": bool, "default": True},
         {"key": "save_images", "type": bool, "default": False},
         {"key": "alwayson_scripts", "type": dict, "default": {}},
+        {"key": "canny_image", "type":str, "default":None}
     ]
 ).generate_model()
 
--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=.\venv\Scripts\python.exe
 set GIT=
 set VENV_DIR=venv
-set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers --no-half
+set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers --no-half  --listen  --port 9092 --enable-insecure-extension-access --api
 
 call webui.bat
