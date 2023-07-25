188df5f491c751504d2cf652ed64a3c39c245e3d
TaoGe <6657718+yowenter@users.noreply.github.com>
2023-07-25 20:53:15 +0800

    add server name

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=.\venv\Scripts\python.exe
 set GIT=
 set VENV_DIR=venv
-set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers --no-half  --listen  --port 9092 --enable-insecure-extension-access --api
+set COMMANDLINE_ARGS=--medvram --autolaunch --deepdanbooru --xformers --no-half  --listen --server-name 192.168.0.123  --port 9092 --enable-insecure-extension-access --api
 
 call webui.bat
